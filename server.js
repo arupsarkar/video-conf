54081714a642545b102e509b46dc524d92943d9e
arupsarkar <sarkar.arup@gmail.com>
2020-04-11 17:52:44 -0400

    removed webpack script

--- server.js
+++ server.js
@@ -5,19 +5,9 @@ var webpack = require("webpack");
 var faker = require("faker");
 
 var app = express();
-if(process.env.NODE_ENV === "DEV") { // Configuration for development environment
-    var webpackDevMiddleware = require("webpack-dev-middleware");
-    var webpackHotMiddleware = require("webpack-hot-middleware");
-    var webpackConfig = require("./webpack.config.js");
-    const webpackCompiler = webpack(webpackConfig);
-    app.use(webpackDevMiddleware(webpackCompiler, {
-      hot: true
-    }));
-    app.use(webpackHotMiddleware(webpackCompiler));
-    app.use(express.static(path.join(__dirname, "app")));
-} else if(process.env.NODE_ENV === "PROD") { // Configuration for production environment
-    app.use(express.static(path.join(__dirname, "dist")));
-}
+
+console.log(new Date(), ' Environment : ' + process.env.NODE_ENV);
+app.use(express.static(path.join(__dirname, "dist")));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
