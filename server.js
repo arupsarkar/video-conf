ee6292c6f23812d5181bdc3e36a91477b238b8de
arupsarkar <sarkar.arup@gmail.com>
2020-04-11 17:59:47 -0400

    remobving web pck

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
-var path = require("path");
-var express = require("express");
-var webpack = require("webpack");
-var faker = require("faker");
+const path = require("path");
+const express = require("express");
+// var webpack = require("webpack");
+// var faker = require("faker");
 
-var app = express();
+const app = express();
 
 console.log(new Date(), ' Environment : ' + process.env.NODE_ENV);
 app.use(express.static(path.join(__dirname, "dist")));
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log("Express server listening on *:" + port);
 });
